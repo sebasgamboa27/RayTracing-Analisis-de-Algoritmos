2920deffa394bc4cabab363bf4632ccefc0bc472
SebasG <gamboasebas@gmail.com>
2020-06-11 20:27:37 -0600

    Raytracing rays with color transparency

--- particle.py
+++ particle.py
@@ -22,57 +22,80 @@ class Particle:
         for i in range(0,len(self.rays)):
 
             if i<len(self.rays)-1:
-                points_first_half=[]
-                points_second_half = []
+                points1=[]
+                points2 = []
+                points3 = []
+                points4 = []
                 points_end = []
 
-                half_start_x1 = self.rays[i].end[0] - (((self.rays[i].end[0]-self.rays[i].pos[0])/ 3)*1 )
-                half_start_y1= self.rays[i].end[1] - (((self.rays[i].end[1]-self.rays[i].pos[1])/ 3)*1 )
 
-                half_start_x2 = self.rays[i+1].end[0] - (((self.rays[i+1].end[0]-self.rays[i+1].pos[0])/ 3)*1 )
-                half_start_y2 = self.rays[i+1].end[1] - (((self.rays[i+1].end[1]-self.rays[i+1].pos[1])/ 3)*1 )
+                half_start_x1 = self.rays[i].end[0] - (((self.rays[i].end[0]-self.rays[i].pos[0])/ 5)*1 )
+                half_start_y1= self.rays[i].end[1] - (((self.rays[i].end[1]-self.rays[i].pos[1])/ 5)*1 )
+                half_start_x2 = self.rays[i+1].end[0] - (((self.rays[i+1].end[0]-self.rays[i+1].pos[0])/ 5)*1 )
+                half_start_y2 = self.rays[i+1].end[1] - (((self.rays[i+1].end[1]-self.rays[i+1].pos[1])/ 5)*1 )
 
-                half_end_x1 = self.rays[i].end[0] - (((self.rays[i].end[0]-self.rays[i].pos[0])/ 3)*2 )
-                half_end_y1 = self.rays[i].end[1] - (((self.rays[i].end[1]-self.rays[i].pos[1])/ 3)*2 )
+                half_end_x1 = self.rays[i].end[0] - (((self.rays[i].end[0]-self.rays[i].pos[0])/ 5)*2 )
+                half_end_y1 = self.rays[i].end[1] - (((self.rays[i].end[1]-self.rays[i].pos[1])/ 5)*2 )
+                half_end_x2 = self.rays[i+1].end[0] - (((self.rays[i+1].end[0]-self.rays[i+1].pos[0])/ 5)*2 )
+                half_end_y2 = self.rays[i+1].end[1] - (((self.rays[i+1].end[1]-self.rays[i+1].pos[1])/ 5)*2 )
 
-                half_end_x2 = self.rays[i+1].end[0] - (((self.rays[i+1].end[0]-self.rays[i+1].pos[0])/ 3)*2 )
-                half_end_y2 = self.rays[i+1].end[1] - (((self.rays[i+1].end[1]-self.rays[i+1].pos[1])/ 3)*2 )
+                pointx3_1 = self.rays[i].end[0] - (((self.rays[i].end[0] - self.rays[i].pos[0]) / 5) * 3)
+                pointy3_1 = self.rays[i].end[1] - (((self.rays[i].end[1] - self.rays[i].pos[1]) / 5) * 3)
+                pointx3_2 = self.rays[i + 1].end[0] - (((self.rays[i + 1].end[0] - self.rays[i + 1].pos[0]) / 5) * 3)
+                pointy3_2 = self.rays[i + 1].end[1] - (((self.rays[i + 1].end[1] - self.rays[i + 1].pos[1]) / 5) * 3)
 
+                pointx4_1 = self.rays[i].end[0] - (((self.rays[i].end[0] - self.rays[i].pos[0]) / 5) * 4)
+                pointy4_1 = self.rays[i].end[1] - (((self.rays[i].end[1] - self.rays[i].pos[1]) / 5) * 4)
+                pointx4_2 = self.rays[i + 1].end[0] - (((self.rays[i + 1].end[0] - self.rays[i + 1].pos[0]) / 5) * 4)
+                pointy4_2 = self.rays[i + 1].end[1] - (((self.rays[i + 1].end[1] - self.rays[i + 1].pos[1]) / 5) * 4)
 
-                half_start1=[half_start_x1,half_start_y1]
+                half_start1 = [half_start_x1, half_start_y1]
                 half_start2 = [half_start_x2, half_start_y2]
 
                 half_end1 = [half_end_x1,half_end_y1]
                 half_end2 = [half_end_x2, half_end_y2]
 
-                points_first_half.append(self.rays[i].pos)
-                points_first_half.append(half_end1)
-                points_first_half.append(half_end2)
+                point3_1 = [pointx3_1, pointy3_1]
+                point3_2 = [pointx3_2, pointy3_2]
 
-                points_second_half.append(half_start2)
-                points_second_half.append(half_start1)
-                points_second_half.append(half_end1)
-                points_second_half.append(half_end2)
+                point4_1 = [pointx4_1, pointy4_1]
+                point4_2 = [pointx4_2, pointy4_2]
 
+                points1.append(self.rays[i].pos)
+                points1.append(point4_1)
+                points1.append(point4_2)
+
+                points2.append(point3_2)
+                points2.append(point3_1)
+                points2.append(point4_1)
+                points2.append(point4_2)
+
+                points3.append(point3_2)
+                points3.append(point3_1)
+                points3.append(half_end1)
+                points3.append(half_end2)
+
+                points4.append(half_start2)
+                points4.append(half_start1)
+                points4.append(half_end1)
+                points4.append(half_end2)
 
                 points_end.append(half_start1)
                 points_end.append(half_start2)
                 points_end.append(self.rays[i+1].end)
                 points_end.append(self.rays[i].end)
 
+                first_color = pygame.Color(255,255,255,200)
+                second_color = pygame.Color(255, 255, 255, 130)
+                third_color = pygame.Color(255, 255, 255, 100)
+                fourth_color = pygame.Color(255, 255, 255, 50)
+                fifth_color = pygame.Color(255, 255, 255, 20)
 
-                texture = pygame.image.load("img/light_small.png").convert_alpha()
-                pygame.transform.scale(texture,(100,100))
-
-                texture_dim = pygame.image.load("img/dimmer3.png").convert_alpha()
-                pygame.transform.scale(texture_dim, (100, 100))
-
-                texture_dim2 = pygame.image.load("img/dimmer4.png").convert_alpha()
-                pygame.transform.scale(texture_dim2, (100, 100))
-
-                pygame.gfxdraw.textured_polygon(game.fog,points_first_half,texture,0,0)
-                pygame.gfxdraw.textured_polygon(game.fog, points_second_half, texture_dim, 0, 0)
-                pygame.gfxdraw.textured_polygon(game.fog, points_end, texture_dim2, 0, 0)
+                pygame.gfxdraw.filled_polygon(game.fog,points1,first_color)
+                pygame.gfxdraw.filled_polygon(game.fog, points2, second_color)
+                pygame.gfxdraw.filled_polygon(game.fog, points3, third_color)
+                pygame.gfxdraw.filled_polygon(game.fog, points4, fourth_color)
+                pygame.gfxdraw.filled_polygon(game.fog, points_end, fifth_color)
 
         screen.blit(game.fog, (0, 0), special_flags=pygame.BLEND_MULT)
 
